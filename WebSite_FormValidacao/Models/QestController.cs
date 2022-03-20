b0a5049bf3c328f9a9ec66a9af2654e13b6e09d6
Pedro <pe_rodnatal@outlook.com>
2022-03-20 11:20:30 -0300

    first commit

---
+++ WebSite_FormValidacao/Models/QestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite_FormValidacao.Models
+{
+    public class QestController : Controller
+    {        public string cod { get; set; }
+
+        [Required(ErrorMessage = "Nome obrigatório")]
+        [DisplayName("Nome do cliente")]
+        public string nome { get; set; }
+    }
+}
